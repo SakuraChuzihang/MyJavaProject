6408b41d16082b4d6bf541ecfaf128417e5e9869
小李 <2867248268@qq.com>
2025-01-19 13:30:12 +0800

    disanci

--- src/com/extend/test01/FatherC.java
+++ src/com/extend/test01/FatherC.java
@@ -1,8 +1,8 @@
 package com.extend.test01;
 
 public class FatherC {
-    public Static void main(String[] args)
+    public static void main(String[] args)
     {
-
+        System.out.println("这是来测试一下我的github是否push成功");
     }
 }
